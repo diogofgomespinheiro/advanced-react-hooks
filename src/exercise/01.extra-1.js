56ef3e8279ea21d9ee78420532f861860fdca227
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-02-06 18:18:00 +0000

    finish ex01.extra-1

---
+++ src/exercise/01.extra-1.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+
+const countReducer = (state, step) => {
+  return state + step
+}
+
+function Counter({initialCount = 0, step = 1}) {
+  const [count, setCount] = React.useReducer(countReducer, initialCount)
+  const increment = () => setCount(step)
+  return <button onClick={increment}>{count}</button>
+}
+
+function App() {
+  return <Counter />
+}
+
+export default App
